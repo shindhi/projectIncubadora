c737977fcc28b8909d3210c8a362ee4078f44086
Diogo Naka <diogo.naka@invillia.com>
2019-12-09 10:43:20 -0300

    Add exception

--- src/main/java/com/invillia/projectSpring/exceptions/NotFoundException.java
+++ src/main/java/com/invillia/projectSpring/exceptions/NotFoundException.java
@@ -3,6 +3,6 @@ package com.invillia.projectSpring.exceptions;
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException(final String message){
-        super("Time não encontrado, ID: "+ message);
+        super("Time não encontrado, ID: " + message);
     }
 }
